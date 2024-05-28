3bdf843e809c9dd5e2296af493844f67d37666d3
Raul Jimenez <rjipefls@gmail.com>
2024-05-28 15:04:53 +0200

    Segundo commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),),
+    );
+  }
+}
